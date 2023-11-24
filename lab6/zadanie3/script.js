c066f84030fee7e7adf8c1cc4f556db7f1e5b1a9
Piotr Karaś <piotr.karas@spoton.com>
2023-11-24 16:02:36 +0100

    Completed lab 6

--- lab6/zadanie3/script.js
+++ lab6/zadanie3/script.js
@@ -1,110 +1,97 @@
-async function fetchData() {
-    try {
-        const response = await fetch('source/city.json');
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error('Błąd podczas pobierania danych:', error);
-    }
-}
-
-async function displayMalopolskieCities() {
-    const data = await fetchData();
-    const malopolskieCities = data.filter(city => city.province === 'małopolskie');
-
-    const malopolskieList = document.getElementById('malopolskieList');
-    malopolskieCities.forEach(city => {
+async function displayTaskA(data) {
+    const taskAInfo = document.getElementById('task-a-info');
+    data.filter(city => city.province === 'małopolskie').forEach(city => {
         const listItem = document.createElement('li');
         listItem.textContent = city.name;
-        malopolskieList.appendChild(listItem);
+        taskAInfo.appendChild(listItem);
     });
 }
 
-async function displayCitiesWithTwoA() {
-    const data = await fetchData();
-    const twoACities = data.filter(city => {
+async function displayTaskB(data) {
+    const taskBInfo = document.getElementById('task-b-info');
+    data.filter(city => {
         const lowercaseName = city.name.toLowerCase();
         const countA = lowercaseName.split('a').length - 1;
         return countA === 2 && lowercaseName.indexOf('a') !== lowercaseName.lastIndexOf('a');
-    });
-
-    const twoAList = document.getElementById('twoAList');
-    twoACities.forEach(city => {
+    }).forEach(city => {
         const listItem = document.createElement('li');
         listItem.textContent = city.name;
-        twoAList.appendChild(listItem);
+        taskBInfo.appendChild(listItem);
     });
 }
 
-async function displayFifthCity() {
-    const data = await fetchData();
+async function displayTaskC(data) {
     const sortedCities = data.sort((a, b) => (b.people / b.area) - (a.people / a.area));
-
-    const fifthCityInfo = document.getElementById('fifthCityInfo');
-    fifthCityInfo.textContent = `Miasto: ${sortedCities[4].name}, Gęstość zaludnienia: ${sortedCities[4].people / sortedCities[4].area}`;
+    const taskCInfo = document.getElementById('task-c-info');
+    taskCInfo.textContent = `${sortedCities[4].name} - gęstość zaludnienia: ${sortedCities[4].people / sortedCities[4].area}`;
 }
 
-async function addCityToAbove100000Cities() {
-    const data = await fetchData();
+async function displayTaskD(data) {
     const above100000Cities = data.filter(city => city.people > 100000);
-
-    const above100000List = document.getElementById('above100000List');
+    const taskDInfo = document.getElementById('task-d-info');
     above100000Cities.forEach(city => {
         const listItem = document.createElement('li');
         listItem.textContent = `${city.name} city`;
-        above100000List.appendChild(listItem);
+        taskDInfo.appendChild(listItem);
     });
 }
 
-async function comparePopulation() {
-    const data = await fetchData();
+async function displayTaskE(data) {
     const above80000Cities = data.filter(city => city.people > 80000);
     const below80000Cities = data.filter(city => city.people <= 80000);
-
-    const populationComparisonInfo = document.getElementById('populationComparisonInfo');
-
-    populationComparisonInfo.textContent = `Liczba miast powyżej 80000 mieszkańców: ${above80000Cities.length}. 
-        Liczba miast poniżej lub równa 80000 mieszkańców: ${below80000Cities.length}.`;
+    const taskEInfo = document.getElementById('task-e-info');
+    taskEInfo.innerHTML = `
+        <p>Liczba miast z ilością mieszkańców większą od 80000: ${above80000Cities.length}.</p>
+        <p>Liczba miast z ilością mieszkańców mniejszą bądź równą 80000: ${below80000Cities.length}.</p>
+    `;
 }
 
-async function calculateAverageAreaForPCities() {
-    const data = await fetchData();
+async function displayTaskF(data) {
     const pCities = data.filter(city => city.township.startsWith('P'));
     const totalArea = pCities.reduce((acc, city) => acc + city.area, 0);
     const averageArea = totalArea / pCities.length;
-
-    const averageAreaInfo = document.getElementById('averageAreaInfo');
-    averageAreaInfo.textContent = `Średnia powierzchnia miast z powiatów zaczynających się na literkę „p”: ${averageArea.toFixed(2)} km².`;
+    const taskFInfo = document.getElementById('task-f-info');
+    taskFInfo.innerHTML = `
+        <p>Średnia powierzchnia miast z powiatów zaczynających się na literę „P”: ${averageArea.toFixed(2)} km².</p>
+    `;
 }
 
 
-async function checkIfPomorskieCitiesAbove5000() {
-
-
-    const data = await fetchData();
+async function displayTaskG(data) {
     const pomorskieCities = data.filter(city => city.province === 'pomorskie');
-
-    if (pomorskieCities.length === 0) {
-        console.log('Brak miast województwa pomorskiego.');
-        return;
-    }
-
     const allAbove5000 = pomorskieCities.every(city => city.people > 5000);
     const above5000CitiesCount = pomorskieCities.filter(city => city.people > 5000).length;
-
-    const pomorskieCitiesInfo = document.getElementById('pomorskieCitiesInfo');
-    pomorskieCitiesInfo.innerHTML = `<p><strong>Czy wszystkie miasta z województwa pomorskiego są większe od 5000 osób?</strong> ${allAbove5000 ? 'Tak' : 'Nie'}.</p><p><strong>Liczba miast województwa pomorskiego większych od 5000 osób:</strong> ${above5000CitiesCount}.</p>`;
-    isButtonCheckIfPomorskieCitiesAbove5000Clicked = true;
+    const taskGInfo = document.getElementById('task-g-info');
+    taskGInfo.innerHTML = `
+        <p>
+            Czy wszystkie miasta z województwa pomorskiego są większe od 5000 osób? 
+            ${allAbove5000 ? 'Tak' : 'Nie'}.
+        </p>
+        <p>
+            Liczba miast województwa pomorskiego większych od 5000 osób: 
+            ${above5000CitiesCount}.
+        </p>
+    `;
 }
 
-function executeOnPageLoad() {
-    displayMalopolskieCities();
-    displayCitiesWithTwoA();
-    displayFifthCity();
-    addCityToAbove100000Cities();
-    comparePopulation();
-    calculateAverageAreaForPCities();
-    checkIfPomorskieCitiesAbove5000();
+async function fetchData() {
+    try {
+        const response = await fetch('source/city.json');
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+    }
 }
 
-executeOnPageLoad();
+async function runTasks() {
+    const data = await fetchData();
+    displayTaskA(data).then(() => console.log('Task A done'));
+    displayTaskB(data).then(() => console.log('Task B done'));
+    displayTaskC(data).then(() => console.log('Task C done'));
+    displayTaskD(data).then(() => console.log('Task D done'));
+    displayTaskE(data).then(() => console.log('Task E done'));
+    displayTaskF(data).then(() => console.log('Task F done'));
+    displayTaskG(data).then(() => console.log('Task G done'));
+}
+
+runTasks().then(() => console.log('All tasks done'));
