908947a4b9a527cab4818bdaa1d8ce10a4515067
Piotr Karaś <piotr.karas@spoton.com>
2023-12-27 22:33:04 +0100

    removed secrets XD

--- tripper/src/environments/environment.ts
+++ tripper/src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyC2xGPPp7_-RAVX2FiNVDxnjfEtS_Fwf4s",
-    authDomain: "tripper-24fa5.firebaseapp.com",
-    projectId: "tripper-24fa5",
-    storageBucket: "tripper-24fa5.appspot.com",
-    messagingSenderId: "453944214083",
-    appId: "1:453944214083:web:659f2d541ca65f48264af6"
+    apiKey: "123",
+    authDomain: "123",
+    projectId: "123",
+    storageBucket: "123",
+    messagingSenderId: "123",
+    appId: "123"
   }
 };
