3ad268902f3c7cdaf5df26d6f4a2a4fb8a04092b
Piotr Karaś <piotr.karas@spoton.com>
2023-12-30 02:17:57 +0100

    added home

--- tripper/src/components/home/home.component.ts
+++ tripper/src/components/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
